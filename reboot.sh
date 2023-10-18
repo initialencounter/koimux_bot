7d3e582ba80c65a2f2e12df635f567f73b141d56
initialencounter <2911583893@qq.com>
2023-10-18 17:19:10 +0000

    chore: change reboot cmd

--- reboot.sh
+++ reboot.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# 启动zowo
+# 启动koishi
 screen -dmS ks
-screen -S ks -p 0 -X stuff "cd /root/zowo; yarn start$(printf \\r)"
-screen -dmS gocq
-screen -S gocq -p 0 -X stuff "cd /root/zowo/gocq; ./go-cqhttp$(printf \\r)"
-# 启动edu_bot
-screen -dmS ks1
-screen -S ks1 -p 0 -X stuff "cd /root/edu_bot; yarn start$(printf \\r)"
-screen -dmS gocq1
-screen -S gocq1 -p 0 -X stuff "cd /root/edu_bot/gocq$(printf \\r)"
-# 启动chatgpt
-screen -dmS ks2
-screen -S ks2 -p 0 -X stuff "cd /root/koimux_bot; yarn start$(printf \\r)"
-screen -dmS gocq2
-screen -S gocq2 -p 0 -X stuff "cd /root/koimux_bot/gocq; ./go-cqhttp$(printf \\r)"
+screen -S ks -p 0 -X stuff "cd /root/koimux_boot; yarn start$(printf \\r)"
