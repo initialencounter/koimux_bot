b845532a31fbbe983a16d28cafa3c4e85925cf00
initialencounter <2911583893@qq.com>
2024-03-15 22:26:58 +0800

    feat: full insatll

--- script/no-proot/full.sh
+++ script/no-proot/full.sh
@@ -1,3 +1,11 @@
+# chromium ref. https://blog.utermux.dev/ut/chromium.html
+pkg i x11-repo -y
+pkg rei tur-repo -y
+pkg i chromium -y
+pkg i ffmpeg -y
+# ref. https://www.reddit.com/r/termux/comments/16dzkhg/libexpatso1_not_found_termux/?rdt=58766
+pkg rei libexpat -y
+
 # 安装nodejs, yarn
 pkg i nodejs-lts -y
 npm config set registry https://registry.npmmirror.com
--- script/no-proot/readme.md
+++ script/no-proot/readme.md
@@ -4,3 +4,8 @@
 ```shell
 bash -c "$(curl -L https://gitee.com/initencunter/koimux_bot/raw/master/script/no-proot/main.sh)"
 ```
+
+附加安装 chromium 和 ffmpeg
+```shell
+bash -c "$(curl -L https://gitee.com/initencunter/koimux_bot/raw/master/script/no-proot/full.sh)"
+```
